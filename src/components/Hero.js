3eb28dddf82b098feb488e377163665be1bc734b
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-01-27 21:52:33 -0800

    fixing video path

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
     <div className="hero-section">
       {/* Video Background */}
       <video className="background-video" autoPlay loop muted playsInline>
-        <source src="/vecteezy_fiber-optic-cables-carrying-information-toward-glowing_2850201.mp4" type="video/mp4" />
+        <source src="/Design/vecteezy_fiber-optic-cables-carrying-information-toward-glowing_2850201.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
