c8c5c5fe2b669fd2a08c01da10c0cc6763e5262c
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-01-31 06:57:12 -0800

    config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Design/', // Remove this if using a custom domain
   plugins: [react()],
 });
